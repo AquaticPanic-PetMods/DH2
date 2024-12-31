6ade4354b533265d8b3f0cac3e3fd327ecbea801
AquaticPanic-PetMods <aquaticpanic@gmail.com>
2024-12-30 21:18:17 -0300

    Update conditions.ts

--- data/mods/Pyramid Panic Lite /conditions.ts
+++ data/mods/Pyramid Panic Lite /conditions.ts
@@ -658,7 +658,7 @@ this.damage(pokemon.baseMaxhp / 4);
 		},
 				onSourceDamagingHit(damage, target, source, move) {
 	if (move.type === 'Fire') {
-				this.randomChance(2, 10) {
+				this.randomChance(2, 10); {
 					target.trySetStatus('brn', source);
 				}
 			}
